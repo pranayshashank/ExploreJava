49be0b56928b55fbdf045265b75518e9bb79fca6
pranayshashank <pranay.shashank@gmail.com>
2018-06-11 19:38:20 +0530

    Miscellaneous

---
+++ in.pks.journal.java.concurrency/src/main/java/in/pks/journal/java/concurrency/LatchBasedTaskExecutor.java
@@ -0,0 +1,56 @@
+package in.pks.journal.java.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * A sample to show, how we can use countdown latch.
+ * Countdown latch is good to use when we want a single time barrier.
+ * The barrier signifies completion of an event or a set of events, post which another dependent task(s) must be executed.
+ * <p>
+ * In this sample, if enabledCountDownLatchBasedExecution = false, then our Daemon threads dies before completing their intended tasks
+ * as main ends before them.
+ * <p>
+ * Since, we wanted main to terminate only after daemon threads are done, we used CountDownLatch.
+ * <p>
+ * PS: Please note, CountDownLatch is not the only way to achieve this, but a convenient way indeed.
+ */
+public class LatchBasedTaskExecutor {
+
+    private CountDownLatch latch = new CountDownLatch(3);
+    private boolean enabledCountDownLatchBasedExecution = true;
+
+    public static void main(String[] args) {
+        LatchBasedTaskExecutor app = new LatchBasedTaskExecutor();
+
+        Thread t1 = new Thread(() -> app.executeTask(app.latch), "DaemonThread-1");
+        Thread t2 = new Thread(() -> app.executeTask(app.latch), "DaemonThread-2");
+        Thread t3 = new Thread(() -> app.executeTask(app.latch), "DaemonThread-3");
+
+        t1.setDaemon(true);
+        t2.setDaemon(true);
+        t3.setDaemon(true);
+        t1.start();
+        t2.start();
+        t3.start();
+
+        while (app.enabledCountDownLatchBasedExecution)
+            try {
+                app.latch.await();
+                break;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        System.out.println(Thread.currentThread().getName() + " finished.");
+    }
+
+    private void executeTask(CountDownLatch latch) {
+        try {
+            System.out.println(Thread.currentThread().getName() + " going to execute.");
+            Thread.sleep(30000);
+            System.out.println(Thread.currentThread().getName() + " executed task.");
+            latch.countDown();
+        } catch (InterruptedException ignore) {
+        }
+    }
+}
---
+++ in.pks.journal.java.concurrency/src/main/java/in/pks/journal/java/concurrency/SemaphoreBasedProducerConsumer.java
@@ -0,0 +1,93 @@
+package in.pks.journal.java.concurrency;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Logger;
+
+public class SemaphoreBasedProducerConsumer {
+
+    Semaphore pub = new Semaphore(1);
+    Semaphore con = new Semaphore(0);
+
+    private static final Logger LOG = Logger.getLogger("SemaphoreBasedProducerConsumer.class");
+
+    ExecutorService prodPool = Executors.newFixedThreadPool(3);
+    ExecutorService consPool = Executors.newFixedThreadPool(3);
+
+    public static void main(String[] args) {
+        SemaphoreBasedProducerConsumer app = new SemaphoreBasedProducerConsumer();
+
+        Queue<String> dataQueue = new ArrayBlockingQueue<>(1);
+        Consumer<String> consumer = new Consumer<>(dataQueue);
+        Producer<String> producer = new Producer<>(dataQueue);
+
+        LOG.info("Yahoo!!");
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            app.prodPool.execute(() -> {
+                Thread.currentThread().setName("Producer-" + Thread.currentThread().getId());
+                boolean produced = false;
+                try {
+                    app.pub.acquireUninterruptibly();
+                    producer.produce("Text-" + finalI);
+                    produced = true;
+                } finally {
+                    if (produced)
+                        app.con.release();
+                }
+            });
+        }
+
+        for (int i = 0; i < 10; i++)
+            app.consPool.execute(() -> {
+                Thread.currentThread().setName("Consumer-" + Thread.currentThread().getId());
+                boolean consumed = false;
+                try {
+                    app.con.acquireUninterruptibly();
+                    consumer.consume();
+                    consumed = true;
+                } finally {
+                    if (consumed)
+                        app.pub.release();
+                }
+            });
+
+        app.prodPool.shutdown();
+        app.consPool.shutdown();
+
+        while (!app.prodPool.isTerminated()) ;
+        while (!app.consPool.isTerminated()) ;
+    }
+
+    public static class Consumer<T> {
+        Queue<T> queue;
+
+        Consumer(Queue<T> queue) {
+            this.queue = queue;
+        }
+
+        public void consume() {
+            T element = this.queue.poll();
+            System.out.println(Thread.currentThread().getName() + "-Consumed-" + element);
+        }
+
+    }
+
+    public static class Producer<T> {
+        Queue<T> queue;
+
+        Producer(Queue<T> queue) {
+            this.queue = queue;
+        }
+
+        public void produce(T element) {
+            this.queue.add(element);
+            System.out.println(Thread.currentThread().getName() + "-Published-" + element);
+        }
+    }
+
+}
